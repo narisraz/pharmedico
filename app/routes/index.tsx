ea947956a7b072782e552b8582bb5fc0fd3cd13c
Naris Razafimahatratra <nrazafimahatratra.ext@softwaymedical.fr>
2022-07-05 10:28:51 +0300

    💬 Change home page text

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,4 @@
+import { Grid, Typography } from "@mui/material";
 import { Link } from "@remix-run/react";
 
 import { useOptionalUser } from "~/utils";
@@ -5,8 +6,20 @@ import { useOptionalUser } from "~/utils";
 export default function Index() {
   const user = useOptionalUser();
   return (
-    <h1>
-      Pharmedico
-    </h1>
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: '100vh' }}
+    >
+
+      <Grid item xs={3}>
+        <Typography variant="h3">Pharmedico</Typography>
+        <Typography variant="h5">Bientôt ! 🚧🚧🚧</Typography>
+      </Grid>   
+      
+    </Grid> 
   );
 }
